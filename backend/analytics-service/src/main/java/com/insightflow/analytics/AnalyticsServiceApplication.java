1ce94376512676dbb0c0ab73c579f1170550956a
Gurumurthys1 <163425568+Gurumurthys1@users.noreply.github.com>
2026-05-23 19:55:17 +0530

    add MongoDB cloud

--- backend/analytics-service/src/main/java/com/insightflow/analytics/AnalyticsServiceApplication.java
+++ backend/analytics-service/src/main/java/com/insightflow/analytics/AnalyticsServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 @EnableCaching
-@SpringBootApplication(scanBasePackages = {"com.insightflow.analytics", "com.insightflow.common"})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}, scanBasePackages = {"com.insightflow.analytics", "com.insightflow.common"})
 public class AnalyticsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsServiceApplication.class, args);
